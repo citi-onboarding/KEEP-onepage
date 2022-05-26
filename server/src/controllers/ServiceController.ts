cd0d6112a60cf6bb33a835a011bc60be95e14749
sthecabral <sthecabral17@gmail.com>
2022-05-26 12:15:08 -0300

    feature(crud-services):get, put and delete methods added

--- server/src/controllers/ServiceController.ts
+++ server/src/controllers/ServiceController.ts
@@ -15,4 +15,32 @@ export default class ServiceController implements Crud {
 
         return response.status(httpStatus).send({ message });
     }
+
+    async get(request: Request, response: Response){
+        const {httpStatus, values} = await Citi.getAll(Service);
+        return response.status(httpStatus).send(values);
+    }
+
+    async delete(request: Request, response: Response){
+        const { id } = request.params;
+        const {value: serviceFound, message} = await Citi.findByID(Service, id);
+
+        if(!serviceFound) return response.status(400).send({ message });
+
+        const { httpStatus, messageFromDelete} = await Citi.deleteValue(Service, serviceFound);
+        return response.status(httpStatus).send({ messageFromDelete});
+    }
+
+    async update(request: Request, response: Response){
+        const { id } = request.params;
+        const {image, title, description} = request.body;
+
+        const isAnyUndefined = Citi.areValuesUndefined(image, title, description, id);
+        if (isAnyUndefined) return response.status(400).send();
+
+        const serviceWithUpdatedValues = { image, title, description};
+
+        const {httpStatus, messageFromUpdate} = await Citi.updateValue(Service, id, serviceWithUpdatedValues);
+        return response.status(httpStatus).send({ messageFromUpdate });
+    }
 }
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,17 +1,12 @@
 import express from 'express';
-import UserController from '@controllers/UserController'
 import ServiceController from '@controllers/ServiceController';
 
 const routes = express.Router();
-const userController = new UserController();
 const serviceController = new ServiceController();
 
-routes.post('/user', userController.create);
-routes.get('/user', userController.get);
-routes.delete('/user/:id', userController.delete);
-routes.put('/user/:id', userController.update);
-
 routes.post('/service', serviceController.create);
-
+routes.get('/service', serviceController.get);
+routes.delete('/service/:id', serviceController.delete);
+routes.put('/service/:id', serviceController.update);
 
 export default routes;
