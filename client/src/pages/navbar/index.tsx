9802f5435216c0958ca34264da1af7184d7a6ce2
aquino2 <eduarda.aquino@ufpe.com>
2022-05-31 15:32:00 -0300

    feature(navbar):frond_da_navbar

---
+++ client/src/pages/navbar/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { NavbarContainer } from './styles';
+import { LogoCITi } from '../../assets';
+
+
+export const Navbar: React.FC = () => {
+    return (
+        <NavbarContainer>
+            <div>
+                
+            </div>
+        </NavbarContainer>
+    );
+}
