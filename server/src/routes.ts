0c984878ea150160b7cc1b8fbac77f20c33b57c9
aquino2 <eduarda.aquino@ufpe.com>
2022-06-02 20:08:33 -0300

    feature(crud-grid2):correção na hora de enviar o models e controllers

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,29 +1,21 @@
 import express from 'express';
-import UserController from '@controllers/UserController'
 import DesignController from '@controllers/DesignController'
 import PhotographyController from '@controllers/PhotographyController'
 
 const routes = express.Router();
-const userController = new UserController();
 const designController = new DesignController();
 const photographyController = new PhotographyController();
 
 
-routes.post('/user', userController.create);
-routes.get('/user', userController.get);
-routes.delete('/user/:id', userController.delete);
-routes.put('/user/:id', userController.update);
+routes.post('/design', designController.create);
+routes.get('/design', designController.get);
+routes.delete('/design/:id', designController.delete);
+routes.put('/design/:id', designController.update);
 
 
-routes.post('/design', userController.create);
-routes.get('/design', userController.get);
-routes.delete('/design/:id', userController.delete);
-routes.put('/design/:id', userController.update);
-
-
-routes.post('/photography', userController.create);
-routes.get('/photography', userController.get);
-routes.delete('/photography/:id', userController.delete);
-routes.put('/photography/:id', userController.update);
+routes.post('/photography', photographyController.create);
+routes.get('/photography', photographyController.get);
+routes.delete('/photography/:id', photographyController.delete);
+routes.put('/photography/:id', photographyController.update);
 
 export default routes;
