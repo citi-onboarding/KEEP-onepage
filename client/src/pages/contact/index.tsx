485ec3680598e41863273973e784ca28d406d2b6
Pedro Santiago <pedrouser19@gmail.com>
2022-06-02 14:57:53 -0300

    created contact page

--- client/src/pages/contact/index.tsx
+++ client/src/pages/contact/index.tsx
@@ -1,20 +1,39 @@
 import React from "react";
-import { ContactContainer } from './styles';
 
+import { ContactContainer, Form } from './styles';
+import { Vector1, Vector2, Vector3, Vector4 } from '../../assets';
 
 export const Contact: React.FC = () => {
     return (
         <ContactContainer>
-         <h1>Contato</h1>  
+            <div className="sidea">
+                <img src={Vector1} className="vector1" />
+                <img src={Vector3} className="vector3" />
+            </div>
+            <Form>
+                <h1>Contato</h1>   
+                <label>Nome</label>
+                <input type="text" id="name" name="user_name"></input>
+                <label>Telefone</label>
+                <input type="phone" id="phone" name="phone"></input>
+                <label>O que você deseja fazer?</label>
+                
+                <select style={{color: 'white'}}>
+                    <option style={{display: 'none'}} value="0" ></option>
+                    <option style={{color: 'black'}} value="1" >Option 1</option>
+                    <option style={{color: 'black'}} value="2">Option 2</option>
+                    <option style={{color: 'black'}} value="3">Option 3</option>
+                    </select>
+        
+                <label>Nos conte a sua ideia! </label>
+                <textarea id="text" name="text"></textarea>
 
-         <p>Nome</p> 
-
-         <p>Telefone</p> 
-
-         <p>O que você deseja?</p> 
-
-         <p>Nos conte a sua ideia!</p> 
-            
+                <button type="button">Enviar</button>
+            </Form> 
+            <div className="sideb">
+                <img src={Vector2} className="vector2" />
+                <img src={Vector4} className="vector4" /> 
+            </div>   
         </ContactContainer>
     );
 }
