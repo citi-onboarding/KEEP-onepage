4f0be0d443b6ef3baa36334962bbedc5d1c9bf04
Pedro Santiago <pedrouser19@gmail.com>
2022-06-03 10:22:43 -0300

    update on style archive

--- client/src/pages/aboutus/index.tsx
+++ client/src/pages/aboutus/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 
-import { AboutUsContainer } from './styles';
+import { AboutUsContainer, Content } from './styles';
 import {  } from '../../assets';
 
 export const AboutUs: React.FC = () => {
     return (
         <AboutUsContainer> 
-            <h1>Teste</h1>
+            <Content>
+                <h1>Teste</h1>
+            </Content>
         </AboutUsContainer>
     );
 }
