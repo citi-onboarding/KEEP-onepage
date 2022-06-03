fff2acf3d07e16a0832c7e9a0a1fc9ad23927493
Pedro Santiago <pedrouser19@gmail.com>
2022-06-03 12:46:13 -0300

    about us section finished

--- client/src/pages/aboutus/index.tsx
+++ client/src/pages/aboutus/index.tsx
@@ -1,44 +1,37 @@
 import React from "react";
 
-import { AboutUsContainer, Content } from './styles';
+import { AboutUsContainer } from './styles';
 import { Audiovisual, Design, Fotografia } from '../../assets';
 
 export const AboutUs: React.FC = () => {
     return (
         <AboutUsContainer> 
-            <Content>
+            
                 <div className="sidea">
-                <h1>Frase impactante sobre quem é a keep e missão</h1>
+                    <h1>Frase impactante sobre quem é a keep e missão</h1>
                 </div>
                 <div className="sideb">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore na aliqua.<br></br>
-                <br></br>
-                Serviços que oferecemos:</p>
-
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore na aliqua.<br></br>
+                    <br></br></p>
+                    <p style={{fontWeight: 600, fontStyle: 'italic'}}>Serviços que oferecemos:</p>
                 <div className="services">
-                    <div className="audiovisual">
-                        <img style={{paddingLeft: 15}} src={Audiovisual}/>
-                        <h2>Audiovisual</h2>
-                    </div>
-
-                    <div className="design">
-                        <img style={{paddingLeft: 40}} src={Design}/>
-                        <h2>Design</h2>
-                    </div>
-                    <div className="fotografia">
-                        <img style={{paddingLeft: 40}} src={Fotografia}/>
-                        <h2>Fotografia</h2>
-                    </div>
-                    
+                    <img style={{paddingLeft: 15, paddingTop: 53}} src={Audiovisual}/>
+                    <img style={{paddingLeft: 59, paddingTop: 40}} src={Design}/>
+                    <img style={{paddingLeft: 69, paddingTop: 45}} src={Fotografia}/> 
+                </div>
+                <div className="servicesdescription">
+                    <h2>Audiovisual</h2>
+                    <h2 style={{paddingLeft: 40}}>Design</h2>
+                    <h2 style={{paddingLeft: 40}}>Fotografia</h2>
                 </div>
 
                 <button type="button">Quero contratar</button>
 
                 </div>
-            </Content>
+            
         </AboutUsContainer>
     );
 }
--- client/src/pages/aboutus/styles.ts
+++ client/src/pages/aboutus/styles.ts
@@ -2,32 +2,27 @@ import styled from 'styled-components';
 
 
 export const AboutUsContainer = styled.div`
- 
-
   background-color: #141414;
-`;
-
-export const Content = styled.div`
   display: flex;
-  margin-left: 7%;
-  margin-right: 7%;
   flex-direction: row;
-  justify-content: space-between;
+  justify-content: center;
 
   .sidea{
-    display: flex;
     flex-direction: column;
-
     width: 426px;
     height: 114px;
-
     margin-top: 193px;
     margin-bottom: 338px;
+    margin-right: 203px;
+    
   }
 
   .sideb{
-    display: flex;
     flex-direction: column;
+    width: 497px;
+    height: 132px;
+    margin-top: 120px;
+    
   }
 
   h1{
@@ -35,27 +30,30 @@ export const Content = styled.div`
 
     font-family: 'Monument Extended';
     font-style: normal;
+    font-weight: 400;
+    font-size: 32px;
+    line-height: 38px;
+
+    color: #FFFFFF;
+
   }
 
   p{
-    width: 497px;
-    height: 132px;
-
-    margin-top: 120px;
-
-    font-family: 'Barlow';
-    font-style: normal;
+    font-family: 'Barlow', sans-serif;
     font-weight: 400;
     font-size: 18px;
-    line-height: 22px;
-
+    line-height: 21.6px;
     color: #FFFFFF;
   }
 
   .services{
     display: flex;
     justify-content: row;
-    margin-top: 40px;
+  }
+
+  .servicesdescription{
+    display: flex;
+    justify-content: row;
   }
 
   h2{
@@ -63,29 +61,36 @@ export const Content = styled.div`
     font-style: normal;
     font-weight: 400;
     font-size: 14px;
-    line-height: 17px;
+    line-height: 16.8px;
+    padding-top: 16px;
   }
-
-  .audiovisual{
-
-  }
-
-
-
 
   button{
     width: 212px;
     height: 33px;
     background: transparent;
     color: white;
-
     margin-top: 64px;
     border: 2px solid #F5F5F5;
-
     font-family: 'Monument Extended';
     font-style: normal;
     font-weight: 400;
     font-size: 14px;
     line-height: 17px;
+    color: #F5F5F5;
   }
+
+  button:hover{
+    background: white;
+    color: #141414;
+  }
+
+  :active{
+    button{
+      background-color: #1A1A1A;
+      border: #1A1A1A;
+      color: #FFFFFF;
+    }
+  }
+  
 `;
