ff7a2dcea78d390662a6affc2296f15bcf57c71f
mariabdma <mariabma76@gmail.com>
2022-06-03 18:18:25 -0300

    Feature(contact): exported keep logo

--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -4,8 +4,8 @@ import Vector2 from './vector2.png';
 import Vector3 from './vector3.png';
 import Vector4 from './vector4.png';
 import Union from './union.png';
-
+import LogoKEEP from './KEEP[NEGATIVA].png';
 
 export {
-    LogoCITi, Vector1, Vector2, Vector3, Vector4, Union
+    LogoCITi, Vector1, Vector2, Vector3, Vector4, Union, LogoKEEP
 };
