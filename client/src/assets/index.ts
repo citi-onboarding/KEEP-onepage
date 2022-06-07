5c0815bb75b83cfd13457fbcb1af884562c3ee24
Pedro Santiago <pedrouser19@gmail.com>
2022-06-07 18:40:11 -0300

    logo deleted

--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -1,8 +1,7 @@
-import LogoCITi from './logo.png';
 import Audiovisual from './audiovisual.png'
 import Design from './design.png'
 import Fotografia from './fotografia.png'
 
 export {
-    LogoCITi, Audiovisual, Design, Fotografia
+    Audiovisual, Design, Fotografia
 };
